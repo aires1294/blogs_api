a4cbd41791b2bb32371006d004a0e19bbc0d33c2
aires1294 <pedrotja.engenharia@gmail.com>
2023-03-06 19:02:23 -0300

    item 2 feitoooo

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+      'User',
+      {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+      },
+      {
+        tableName: 'users',
+        timestamps: false,
+        underscored: true,
+      },
+    );
+    User.associate = (models) => {
+      User.hasMany(models.Product, {
+        foreignKey: 'id',
+        as: 'user',
+      });
+    };
+    return User;
+  };
+  
