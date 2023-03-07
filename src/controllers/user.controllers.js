20ee866b894cff44de3a56dcfbb2ff52951a66c1
aires1294 <pedrotja.engenharia@gmail.com>
2023-03-07 19:06:34 -0300

    item 3 finalizado a refatoração

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -10,10 +10,6 @@ const jwtConfig = {
 const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await userService.login(email, password);
-    
-    if (!user) {
-        return res.status(400).json({ message: 'Invalid fields' });
-    }
 
     const token = jwt.sign({ data: { userId: user.id } }, secret, jwtConfig);
     res.status(200).json({ token });
--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -1,3 +1,5 @@
+const userService = require('../services/user.service');
+
 const validateLoginBody = (req, res, next) => {
     const { email, password } = req.body;
     if (!email || !password) {
@@ -6,6 +8,16 @@ const validateLoginBody = (req, res, next) => {
     next();
 };
 
+const validateUser = async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await userService.login(email, password); 
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid fields' });
+    }
+    next();
+};
+
 module.exports = {
     validateLoginBody,
+    validateUser,
 };
